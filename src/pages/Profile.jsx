3b0255f2446af29cb73d7b42f91d869ea92b3774
Tymur Korzhakov <7umypka@gmail.com>
2022-12-15 14:19:10 +0100

    add profile edit functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { updateDoc, doc } from "firebase/firestore";
+import { db } from "../firebase";
 
 export const Profile = () => {
   const navigate = useNavigate();
   const auth = getAuth();
+  const [changeDetail, setChangeDetail] = useState();
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -14,6 +18,32 @@ export const Profile = () => {
   const onLogout = () => {
     auth.signOut();
     navigate("/");
+  };
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        //update displayName in firebase auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        //update name in firestore
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success("Profile updated");
+    } catch (error) {
+      toast.error("Could not update profile details");
+    }
   };
   return (
     <>
@@ -25,22 +55,32 @@ export const Profile = () => {
               type='text'
               id='name'
               value={name}
-              disabled
-              className='w-full px-4 py-2 text-xl text-gray-700 bg-gray-50 border border-gray-300 rounded transition ease-in-out mb-4'
+              disabled={!changeDetail}
+              onChange={onChange}
+              className={`w-full px-4 py-2 text-xl text-gray-700 bg-gray-50 border border-gray-300 rounded transition ease-in-out mb-4 ${
+                changeDetail && "bg-green-100 focus:bg-green-100"
+              }`}
             />
             <input
               type='email'
               id='email'
               value={email}
-              disabled
-              className='w-full px-4 py-2 text-xl text-gray-700 bg-gray-50 border border-gray-300 rounded transition ease-in-out'
+              disabled={!changeDetail}
+              onChange={onChange}
+              className={`w-full px-4 py-2 text-xl text-gray-700 bg-gray-50 border border-gray-300 rounded transition ease-in-out ${
+                changeDetail && "bg-green-100 focus:bg-green-100"
+              }`}
             />
             <div className='flex justify-between pt-6'>
               <button
+                onClick={() => {
+                  changeDetail && onSubmit();
+                  setChangeDetail((prevState) => !prevState);
+                }}
                 type='button'
                 className='bg-blue-500 rounded-md p-3 text-white'
               >
-                Edit profile
+                {changeDetail ? "Apply changes" : "Edit profile"}
               </button>
               <button
                 onClick={onLogout}
