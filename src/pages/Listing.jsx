5c8980e9b4e2862a55aa38a3b97ded85c94c5149
Tymur Korzhakov <7umypka@gmail.com>
2022-12-21 15:27:25 +0100

    add share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,11 +6,13 @@ import { db } from "../firebase";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination } from "swiper";
 import "swiper/css/bundle";
+import { RiShareBoxLine } from "react-icons/ri";
 
 export const Listing = () => {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
   SwiperCore.use([Autoplay, Navigation, Pagination]);
 
@@ -43,7 +45,7 @@ export const Listing = () => {
         {listing.imgUrls.map((url, index) => (
           <SwiperSlide key={index}>
             <div
-              className='relative w-full overflow-hidden h-[300px]'
+              className='relative w-full overflow-hidden h-[600px]'
               style={{
                 background: `url(${listing.imgUrls[index]}) center no-repeat`,
                 backgroundSize: "cover",
@@ -52,6 +54,23 @@ export const Listing = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className='fixed top-[13%] right-[3%] z-10 cursor-pointer bg-white border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center hover:scale-110 hover:border-black hover:shadow-xl'
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <RiShareBoxLine className='text-lg text-slate-500 hover:text-black' />
+      </div>
+      {shareLinkCopied && (
+        <p className='fixed top-[20%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2'>
+          Link copied
+        </p>
+      )}
     </main>
   );
 };
